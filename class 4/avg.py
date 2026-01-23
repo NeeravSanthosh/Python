8e5fd49ce748f0a7fffdd5312f0ae0dc98cb92e1
NeeravSanthosh <neeravsanthosh0403@gmail.com>
2026-01-23 17:32:34 +0530

    Add files via upload

---
+++ class 4/avg.py
@@ -0,0 +1,11 @@
+# average
+tree1 = 193
+tree2 = 188
+tree3 = 180
+tree4 = 312
+tree5 = 105
+sum = tree1 + tree2 + tree3 + +tree4 + tree5
+avg = sum/5
+
+print("the total height of the tree", sum)
+print("the average height of the tree", avg)
