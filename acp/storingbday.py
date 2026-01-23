cad3ac1ecaaba74f2879f222655c8d9ac67cc69f
NeeravSanthosh <neeravsanthosh0403@gmail.com>
2026-01-23 17:38:09 +0530

    Add files via upload

---
+++ acp/storingbday.py
@@ -0,0 +1,9 @@
+David = '12/06/2013'
+Alok = '31/12/2012'
+Jordan = '19/04/2011'
+Chico = '27/09/2012'
+
+print("David birthday is on ",David)
+print("Alok birthday is on ",Alok)
+print("Jordan birthday is on ",Jordan)
+print("Chico birthday is on ",Chico)
